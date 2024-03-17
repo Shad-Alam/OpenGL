7235953f35ec3a9d28bd87df0c1a5e71eddd64bb
Shad-Alam <shad.19bd@gmail.com>
2024-03-17 15:39:10 +0600

    adding home layer

--- main.c
+++ main.c
@@ -10,13 +10,13 @@ void triangle()
 {
     glBegin(GL_TRIANGLES);
 		
-		glColor3f(0.0, 0.0, 1.0);
+		glColor3f(0.0f, 0.2f, 0.0f);
 		glVertex2f(0.0,3.0);
 		
-		glColor3f(0.0, 0.0, 1.0);
+		glColor3f(0.0f, 0.2f, 0.0f);
 		glVertex2f(-2.0,7.0);
 		
-		glColor3f(0.0, 0.0, 1.0);
+		glColor3f(0.0f, 0.2f, 0.0f);
 		glVertex2f(-3.0,4.0);
 		
 		
@@ -24,36 +24,74 @@ void triangle()
 
 }
 
+// down of triangle
 void quads1()
 {
     glBegin(GL_QUADS);
-		glColor3f(1.0, 10.0, 1.0);
+		glColor3f(0.5f, 2.0f, 1.0f);
 		glVertex2f(0.0, 3.0);
 		
-		glColor3f(1.0, 10.0, 1.0);
+		glColor3f(0.4f, 2.0f, 1.0f);
 		glVertex2f(-3.0, 4.0);
 		
-		glColor3f(1.0, 10.0, 1.0);
-		glVertex2f(-3.0, 1.0);
+		glColor3f(0.5f, 2.0f, 1.0f);
+		glVertex2f(-3.0, 1.3);
 		
-		glColor3f(0.0, 10.0, 1.0);
+		glColor3f(0.4f, 2.0f, 1.0f);
 		glVertex2f(0.0, 0.0);
 	glEnd();
 }
 
+void downLayer1()
+{
+    glBegin(GL_QUADS);
+    
+		glColor3f(1.0f, 0.0f, 0.0f);
+		glVertex2f(0.0, -0.3);
+		
+		glColor3f(1.0f, 0.0f, 0.0f);
+		glVertex2f(-3.0, 1.0);
+		
+		glColor3f(1.0f, 0.0f, 0.0f);
+		glVertex2f(-3.0, 1.3);
+		
+		glColor3f(1.0f, 0.0f, 0.0f);
+		glVertex2f(0.0, 0.0);
+		
+	glEnd();
+}
+
+// beside left quad
 void quads2()
 {
     glBegin(GL_QUADS);
-		glColor3f(1.0, 0.0, 1.0);
+		glColor3f(0.2f, 1.0f, 1.0f);
 		glVertex2f(0.0, 3.0);
 		
-		glColor3f(1.0, 0.0, 1.0);
+		glColor3f(0.3f, 1.0f, 1.0f);
 		glVertex2f(4.0, 3.0);
 		
-		glColor3f(1.0, 0.0, 1.0);
+		glColor3f(0.3f, 1.0f, 1.0f);
 		glVertex2f(4.0, 0.0);
 		
-        glColor3f(1.0, 0.0, 1.0);
+		glColor3f(0.2f, 1.0f, 1.0f);
+		glVertex2f(0.0, 0.0);
+	glEnd();
+}
+
+void downLayer2()
+{
+    glBegin(GL_QUADS);
+		glColor3f(1.0f, 0.0f, 0.0f);
+		glVertex2f(0.0, -0.3);
+		
+		glColor3f(1.0f, 0.0f, 0.0f);
+		glVertex2f(4.0, -0.3);
+		
+		glColor3f(1.0f, 0.0f, 0.0f);
+		glVertex2f(4.0, -0.0);
+		
+		glColor3f(1.0f, 0.0f, 0.0f);
 		glVertex2f(0.0, 0.0);
 	glEnd();
 }
@@ -61,16 +99,16 @@ void quads2()
 void quads3()
 {
     glBegin(GL_QUADS);
-		glColor3f(0.0, 1.0, 0.0);
+		glColor3f(0.0f, 0.5f, 0.5f);
 		glVertex2f(-2.0, 7.0);
 		
-		glColor3f(0.0, 1.0, 0.0);
-		glVertex2f(3.0, 7.0);
+		glColor3f(0.0f, 0.5f, 0.3f);
+		glVertex2f(2.0, 7.0);
 		
-		glColor3f(0.0, 1.0, 0.0);
+		glColor3f(0.0f, 0.5f, 0.3f);
 		glVertex2f(4.0, 3.0);
 		
-		glColor3f(0.0, 1.0, 0.0);
+		glColor3f(0.0f, 0.5f, 0.3f);
 		glVertex2f(0.0, 3.0);
 	glEnd();
 }
@@ -79,8 +117,8 @@ void quads3()
 void display(void)
 {
 	glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
-
-    glMatrixMode( GL_PROJECTION );
+	
+    glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     gluOrtho2D(-10, 10, -10, 10);
 
@@ -93,6 +131,8 @@ void display(void)
     quads1();
     quads2();
     quads3();
+    downLayer1();
+    downLayer2();
 
     glFlush();
     glutSwapBuffers();
@@ -106,7 +146,7 @@ int main (int argc, char **argv)
 
     glutInitWindowPosition(100,100);
     glutInitWindowSize(windowWidth, windowHeight);
-    glutCreateWindow("Triangle-Demo");
+    glutCreateWindow("2019000000059");
 
     glShadeModel( GL_SMOOTH );
     glEnable( GL_DEPTH_TEST );
@@ -118,30 +158,3 @@ int main (int argc, char **argv)
     return 0;
 }
 
-
-/**
-
-void displayMe(void)
-{
-    glClear(GL_COLOR_BUFFER_BIT);
-    glBegin(GL_POLYGON);
-        glVertex3f(0.5, 0.0, 0.5);
-        glVertex3f(0.5, 0.0, 0.0);
-        glVertex3f(0.0, 0.5, 0.0);
-        glVertex3f(0.0, 0.0, 0.5);
-    glEnd();
-    glFlush();
-}
- 
-int main(int argc, char** argv)
-{
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_SINGLE);
-    glutInitWindowSize(400, 300);
-    glutInitWindowPosition(100, 100);
-    glutCreateWindow("Hello world!");
-    glutDisplayFunc(displayMe);
-    glutMainLoop();
-    return 0;
-}
-*/
